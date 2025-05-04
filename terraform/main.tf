f14e8b4788ce3159b17eda21f7dadf6e1e1a86cc
Atul Kamble <atul_kamble@hotmail.com>
2025-05-04 06:40:01 +0000

    code

---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "cc57cd42-dede-4674-b810-a0fbde41504a"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "aks-resource-group"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-cluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "azuredevopsapp"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
